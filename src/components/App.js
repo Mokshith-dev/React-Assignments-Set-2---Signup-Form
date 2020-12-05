f1a1c09d76000dbbd7c7ad03478645b35767f6f1
Mokshith-dev <68708270+Mokshith-dev@users.noreply.github.com>
2020-12-06 00:58:58 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,108 +1,11 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
+import Login from "./login";
 
 const App = () => {
-  const [name, setName] = useState("");
-  const [userName, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [gender, setGender] = useState("male");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-
-  const onChangeInName = (event) => {
-    setName(event.target.value);
-  };
-  const onChangeInMail = (event) => {
-    setEmail(event.target.value);
-  };
-  const onChangeInGender = (event) => {
-    setGender(event.target.value);
-  };
-  const onChangeInPhoneNumber = (event) => {
-    setPhoneNumber(event.target.value);
-  };
-  const onChangeInPassword = (event) => {
-    setPassword(event.target.value);
-  };
-  const handleSubmit = () => {
-    const alphaNumeric = /^[0-9a-zA-Z]+$/;
-    const numbers = /^\d+$/;
-    setUserName("");
-    console.log(parseInt(phoneNumber));
-    console.log(isNaN(parseInt(phoneNumber)));
-    if (
-      name === "" ||
-      email === "" ||
-      gender === "" ||
-      phoneNumber === "" ||
-      password === ""
-    ) {
-      setError("All fields are mandatory");
-      return;
-    }
-    if (!name.match(alphaNumeric)) {
-      setError("Name is not alphanumeric");
-      return;
-    }
-    if (!email.includes("@")) {
-      setError("Email must contain @");
-      return;
-    }
-    if (!gender) {
-      setError("Please identify as male, female or others");
-    }
-    if (!numbers.test(phoneNumber)) {
-      setError("Phone Number must contain only numbers");
-      return;
-    }
-    if (password.length < 6) {
-      setError("Password must contain atleast 6 letters");
-      return;
-    }
-    setError("");
-    setUserName(email.split("@")[0]);
-  };
   return (
     <div id="main">
-      {error ? <div>{error}</div> : ""}
-      {userName ? <div>Hello {userName}</div> : ""}
-      <input
-        data-testid="name"
-        type="text"
-        placeholder="name"
-        value={name}
-        onChange={onChangeInName}
-      />
-      <input
-        data-testid="email"
-        type="email"
-        placeholder="email"
-        value={email}
-        onChange={onChangeInMail}
-      />
-      <select value={gender} data-testid="gender" onChange={onChangeInGender}>
-        <option value="male">male</option>
-        <option value="female">female</option>
-        <option value="other">other</option>
-      </select>
-      <input
-        data-testid="phoneNumber"
-        type="text"
-        placeholder="Phone Number"
-        value={phoneNumber}
-        onChange={onChangeInPhoneNumber}
-      />
-      <input
-        data-testid="password"
-        type="password"
-        placeholder="password"
-        value={password}
-        onChange={onChangeInPassword}
-      />
-      <button data-testid="submit" onClick={handleSubmit}>
-        Submit
-      </button>
+      <Login />
     </div>
   );
 };
